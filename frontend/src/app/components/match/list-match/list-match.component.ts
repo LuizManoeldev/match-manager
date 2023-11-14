b4410c060b2546c45ca31ce8d873af44c21a4911
Luiz Manoel <dantasluizmanoel@gmail.com>
2023-11-14 19:14:47 -0300

    Atualizações

--- frontend/src/app/components/match/list-match/list-match.component.ts
+++ frontend/src/app/components/match/list-match/list-match.component.ts
@@ -29,9 +29,9 @@ export class ListMatchComponent {
     const stringP = match.esporte
     switch (stringP.toLowerCase()) {
       case "volei":
-        return "https://www.vera.mt.gov.br/fotos_bancoimagens/1120.png"
+        return "../../../../assets/images/volei.png"
       default:
-        return "https://cdn.cnt.org.br/diretorioVirtualPrd/b6506537-0243-4723-be22-2ad8e438078b_CorteRetangular.png"
+        return "../../../../assets/images/soccer.png"
 
     }
   }
