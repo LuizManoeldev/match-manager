fcc8a6b0e954ec84f88cbc06c94975abc5f3e6bb
Luiz Manoel <dantasluizmanoel@gmail.com>
2024-01-08 18:06:37 -0300

    version 1.0.1

--- frontend/src/app/components/match/sorteio-match/sorteio-match.component.ts
+++ frontend/src/app/components/match/sorteio-match/sorteio-match.component.ts
@@ -174,8 +174,6 @@ export class SorteioMatchComponent implements OnInit{
     return jj;
   }
 
-
-
   shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
